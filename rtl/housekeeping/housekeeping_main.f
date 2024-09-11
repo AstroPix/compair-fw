a2d2e457dc37bc155ba6cadc15c684d2af7bc2f9
Richard Leys <richard.leys@kit.edu>
2024-09-11 11:21:56 +0200

    Housekeeping: Restored simple ADC+DAC SPI master, removed useless modules like xilinx xadc interface

--- rtl/housekeeping/housekeeping_main.f
+++ rtl/housekeeping/housekeeping_main.f
@@ -1,4 +1,1 @@
 ${BASE}/rtl/housekeeping/housekeeping_main.sv
-${BASE}/rtl/housekeeping/ext_adcdac_driver.sv
-${BASE}/rtl/housekeeping/xadc_hk_driver.sv
-${BASE}/rtl/housekeeping/xadc_driver_internal.sv
