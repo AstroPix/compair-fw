e5930f4662e9ae76756536d26a379a0538eb6ae4
salman.i.sheikh@nasa.gov <ssheikh@ndc.nasa.gov>
2024-11-21 14:07:48 -0500

    3layer pins modification

--- target-certusnx-devboard/compair_fpga_top.sv
+++ target-certusnx-devboard/compair_fpga_top.sv
@@ -2,13 +2,45 @@ module compair_fpga_top(
 
 	input   wire            sysclk_125, // Clock on N25, JP15 is removed, or output J23 is 1 or hi-z
     input   wire            rstn,
-    
     input   wire            ftdi_tx, // TX of FTDI UART
     output  wire            ftdi_rx, // RX of FTDI UART
 	
-    input   wire            spi_clk,
+   input   wire            spi_clk,
     input   wire            spi_csn,
-   
+	
+	output wire             spi_left_clk,
+	output wire             spi_left_csn,
+	input  wire    [1:0]    spi_left_miso,
+	output wire             spi_left_mosi,
+	
+	output wire             inj,
+	output wire             hold,
+	output wire             res_n,
+	input  wire             interrupt,
+	output wire             sr_rb,
+	
+	//Astropix Sample Clk
+//    output wire             sample_clk,
+    output wire             sample_clk,
+    output wire             sample_clk_se,
+	output wire             clk_timestamp,
+	
+    output wire             sr_ck1, 
+    output wire             sr_ck2, 
+    output wire             sr_ld, 
+    output wire             sr_sin, 
+	input  wire             sr_out,
+
+	
+	output wire             vb_load,
+	output wire             vb_data,
+	output wire             vb_clk,
+	
+	//output wire             ext_spi_adc_csn,
+	//output wire             ext_spi_adc_miso,
+	
+	
+
     output  wire    [7:0]   LED_RED
 );
 
@@ -46,13 +78,13 @@ module compair_fpga_top(
         .ext_spi_mosi(ext_spi_mosi),
 
 
-        .layer_0_hold(),
-        .layer_0_resn(),
-        .layer_0_interruptn(1'b1),
-        .layer_0_spi_clk(),
-        .layer_0_spi_csn(),
-        .layer_0_spi_miso(2'b00),
-        .layer_0_spi_mosi(),
+        .layer_0_hold(hold),
+        .layer_0_resn(res_n),
+        .layer_0_interruptn(interrupt),
+        .layer_0_spi_clk(spi_left_clk),
+        .layer_0_spi_csn(spi_left_csn),
+        .layer_0_spi_miso(spi_left_miso),
+        .layer_0_spi_mosi(spi_left_mosi),
         
 
         // Layers Wiring: Fix layers 1-2 to constants if in single layer mode, otherwise route out
@@ -73,26 +105,26 @@ module compair_fpga_top(
     
 
         // Layers Config
-        .layers_inj(),
-        .layers_spi_csn(),
-        .layers_sr_in_rb(),
-        .layers_sr_in_sout0(1'b0),
+        .layers_inj(inj),    // this is DigInj?
+        .layers_spi_csn(),   // is this no connect
+        .layers_sr_in_rb(sr_rb),
+        .layers_sr_in_sout0(sr_out),
         .layers_sr_in_sout1(1'b0),
         .layers_sr_in_sout2(1'b0),
-        .layers_sr_out_sin(),
-        .layers_sr_out_ck1(),
-        .layers_sr_out_ck2(),
-        .layers_sr_out_ld0(),
+        .layers_sr_out_sin(sr_sin),
+        .layers_sr_out_ck1(sr_ck1),
+        .layers_sr_out_ck2(sr_ck2),
+        .layers_sr_out_ld0(sr_ld),
         .layers_sr_out_ld1(),
         .layers_sr_out_ld2(),
 
         .rfg_io_led(rfg_led),
 
         // SPI Slave interface for HOST
-        .spi_clk(spi_clk),
-        .spi_csn(spi_csn), // always deselect
-        .spi_miso(),
-        .spi_mosi(1'b0),
+        .spi_clk(),
+        .spi_csn(), // always deselect
+        .spi_miso(),       // are we not using this
+        .spi_mosi(1'b0),   // why is this '0'
         
         .uart_rx(ftdi_tx),
         .uart_tx(ftdi_rx),
@@ -100,9 +132,9 @@ module compair_fpga_top(
         .uart_got_byte(),
     
         
-        .gecco_sr_ctrl_ck(),
-        .gecco_sr_ctrl_sin(),
-        .gecco_sr_ctrl_ld(),
+        .gecco_sr_ctrl_ck(vb_clock),   // what are these 3 and following 4 signals
+        .gecco_sr_ctrl_sin(vb_data),
+        .gecco_sr_ctrl_ld(vb_load),
 
         .io_ctrl_sample_clock_enable(),
         .io_ctrl_timestamp_clock_enable(),
---
+++ target-certusnx-devboard/fifo_2clk_64x32/constraints/constraint.sdc
@@ -0,0 +1,74 @@
+set device "LFCPNX-100"
+set device_int "jd5d80"
+set package "LFG672"
+set package_int "LFG672"
+set speed "9_Low-Power_1.0V"
+set speed_int "6"
+set operation "Commercial"
+set family "LFCPNX"
+set architecture "jd5d00"
+set partnumber "LFCPNX-100-9LFG672C"
+set WRAPPER_INST "lscc_fifo_dc_inst"
+set WADDR_DEPTH 64
+set WDATA_WIDTH 32
+set RADDR_DEPTH 64
+set RDATA_WIDTH 32
+set FIFO_CONTROLLER "FABRIC"
+set FWFT 0
+set FORCE_FAST_CONTROLLER 0
+set IMPLEMENTATION "EBR"
+set WADDR_WIDTH 6
+set RADDR_WIDTH 6
+set REGMODE "noreg"
+set OREG_IMPLEMENTATION "LUT"
+set RESETMODE "sync"
+set ENABLE_ALMOST_FULL_FLAG "TRUE"
+set ALMOST_FULL_ASSERTION "static-dual"
+set ALMOST_FULL_ASSERT_LVL 63
+set ALMOST_FULL_DEASSERT_LVL 62
+set ENABLE_ALMOST_EMPTY_FLAG "TRUE"
+set ALMOST_EMPTY_ASSERTION "static-dual"
+set ALMOST_EMPTY_ASSERT_LVL 1
+set ALMOST_EMPTY_DEASSERT_LVL 2
+set ENABLE_DATA_COUNT_WR "TRUE"
+set ENABLE_DATA_COUNT_RD "TRUE"
+set FAMILY "LFCPNX"
+
+
+
+if { $radiant(stage) == "premap" } {
+
+    if {$FAMILY == "LIFCL" | $FAMILY == "LFCPNX" | $FAMILY == "LFD2NX" | $FAMILY == "LFMXO5"} {
+        set WR_CLK_PERIOD 8
+        set RD_CLK_PERIOD 8
+        
+        if {$FIFO_CONTROLLER == "HARD_IP"} {
+            set WR_MAXDLY [expr {$WR_CLK_PERIOD*0.8}]
+            set RD_MAXDLY [expr {$RD_CLK_PERIOD*0.8}]
+            
+            set_max_delay -datapath_only -from [get_cells -hierarchical */*.FIFO16K_MODE_inst] -to [get_cells -hierarchical */*.full_r*.*_inst] $WR_MAXDLY
+            set_max_delay -datapath_only -from [get_cells -hierarchical */*.FIFO16K_MODE_inst] -to [get_cells -hierarchical */*.afull_r*.*_inst] $WR_MAXDLY
+            set_max_delay -datapath_only -from [get_cells -hierarchical */*.FIFO16K_MODE_inst] -to [get_cells -hierarchical */*.empty_r*.*_inst] $RD_MAXDLY
+            set_max_delay -datapath_only -from [get_cells -hierarchical */*.FIFO16K_MODE_inst] -to [get_cells -hierarchical */*.aempty_r*.*_inst] $RD_MAXDLY
+            set_false_path -from [get_pins -hierarchical */*.FIFO16K_MODE_inst/EMPTY] -to [get_pins -hierarchical */*.FIFO16K_MODE_inst/EMPTYI]
+            set_false_path -from [get_pins -hierarchical */*.FIFO16K_MODE_inst/FULL] -to [get_pins -hierarchical */*.FIFO16K_MODE_inst/FULLI] 
+            
+            if {$REGMODE == "reg"} {
+                set_max_delay -datapath_only -from [get_cells -hierarchical */*.empty_r*.*_inst] -to [get_cells -hierarchical */*.empty_sync_r*.*_inst] $RD_MAXDLY
+                set_max_delay -datapath_only -from [get_cells -hierarchical */*.FIFO16K_MODE_inst] -to [get_cells -hierarchical */*.empty_sync_r*.*_inst] $RD_MAXDLY
+                set_max_delay -datapath_only -from [get_cells -hierarchical */*.aempty_r*.*_inst] -to [get_cells -hierarchical */*.aempty_sync_r*.*_inst] $RD_MAXDLY
+                set_max_delay -datapath_only -from [get_cells -hierarchical */*.FIFO16K_MODE_inst] -to [get_cells -hierarchical */*.aempty_sync_r*.*_inst] $RD_MAXDLY
+            }
+        }
+        
+        if {$FIFO_CONTROLLER == "FABRIC" & $IMPLEMENTATION == "LUT" & $radiant(synthesis) == "synpro"} {
+            set_false_path -from [get_pins -hierarchical */_FABRIC.u_fifo/*distmem*.*/DO*] -to [get_pins -hierarchical */_FABRIC.u_fifo/DIST.out_raw*.ff_inst/DF]
+        }
+    
+        if {$FIFO_CONTROLLER == "FABRIC" & $IMPLEMENTATION == "LUT" & $radiant(synthesis) == "lse"} {
+            set_false_path -from [get_pins -hierarchical */_FABRIC.u_fifo/DIST.distmem*.dpram*/DO*] -to [get_pins -hierarchical */_FABRIC.u_fifo/DIST.out_raw*.ff_inst/DF]
+        }
+        
+    }
+
+}
