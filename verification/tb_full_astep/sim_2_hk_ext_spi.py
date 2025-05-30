793be7340f806b1679d61e69e39091f7e365c5b5
richnou <richard.leys@kit.edu>
2025-05-30 09:10:28 +0200

    Simulation: added longer ADC test to read 8 channels at once
    The new test only checks that the firmware is reporting a correct number
    of sampled bytes from spi.
    
    There is a helper method to read X channels from one adc, if I didn't
    misread the datasheet, reading 8 channels at once is possible by sending
    16 bytes, even bytes containing the address of the channel (3 bits, 0-7)

--- verification/tb_full_astep/sim_2_hk_ext_spi.py
+++ verification/tb_full_astep/sim_2_hk_ext_spi.py
@@ -20,7 +20,7 @@ vip.spi.info()
 from vip import astep24_3l_sim
 astep24_3l_sim.info()
 
-@cocotb.test(timeout_time = 3 , timeout_unit = "ms",skip=False)
+@cocotb.test(timeout_time = 5 , timeout_unit = "ms",skip=False)
 async def test_hk_ext_spi_adc(dut):
 
     ## Init Driver
@@ -45,41 +45,76 @@ async def test_hk_ext_spi_adc(dut):
     await driver.houseKeeping.selectADC(0)
 
     ## Read buffer
+    ###
+
+    ## Reported ADC Bytes should be 2
     adcBytesCount = await driver.houseKeeping.getADCBytesCount()
     assert(adcBytesCount == 2)
+
+    ## Get the bytes
     adcBytes = await driver.houseKeeping.readADCBytes(adcBytesCount)
     assert(len(adcBytes) ==2)
+
+    ## Now should be 0 again
+    adcBytesCount = await driver.houseKeeping.getADCBytesCount()
+    assert(adcBytesCount == 0)
     await Timer(5, units="us")
 
     await driver.close()
     await Timer(50, units="us")
 
 
-
-@cocotb.test(timeout_time = 1 , timeout_unit = "ms",skip=True)
-async def test_hk_ext_spi_dac(dut):
+@cocotb.test(timeout_time = 5 , timeout_unit = "ms",skip=False)
+async def test_hk_ext_spi_readnodata(dut):
 
     ## Init Driver
     await vip.cctb.common_clock_reset(dut)
     await Timer(10, units="us")
-    driver = await astep24_3l_sim.getDriver(dut)
+    driver = await astep24_3l_sim.getDriver(dut,spi = False)
 
+    await Timer(50, units="us")
+    adcBytes = await driver.houseKeeping.readADCBytes(8)
+    assert(len(adcBytes) ==8)
+
+    await Timer(50, units="us")
+
+async def read_adc_channels(driver,adc,firstChannel=0,count=8):
+
+    mosiBytes =[]
+    for i in range(count):
+        mosiBytes += [firstChannel + i , 0x00]
+
+    await driver.houseKeeping.writeADCDACBytes(mosiBytes)
+    await driver.houseKeeping.selectADC(adc)
+    await driver.houseKeeping.selectADC(0)
+
+    # Get the bytes
+    adcBytesCount = await driver.houseKeeping.getADCBytesCount()
+    assert(adcBytesCount == len(mosiBytes))
+    adcBytes = await driver.houseKeeping.readADCBytes(adcBytesCount)
+    assert(len(adcBytes) ==len(mosiBytes))
+
+    return adcBytes
+
+
+
+@cocotb.test(timeout_time = 20 , timeout_unit = "ms",skip=False)
+async def test_hk_ext_spi_adc_readchannels(dut):
+
+    ## Init Driver
+    await vip.cctb.common_clock_reset(dut)
+    await Timer(10, units="us")
+    driver = await astep24_3l_sim.getDriver(dut,spi = False)
 
     ## Create VIP SPi Slave
-    slave = vip.spi.VSPISlave(clk = dut.ext_spi_clk, csn = dut.ext_dac_spi_csn,mosi=dut.ext_spi_mosi,miso = dut.ext_adc_spi_miso,misoSize=1,cpol=0)
+    slave = vip.spi.VSPISlave(clk = dut.ext_spi_clk, csn = dut.ext_adc_spi_csn[0],mosi=dut.ext_spi_mosi,miso = dut.ext_adc_spi_miso,misoSize=1,cpol=0)
     slaveTask = slave.start_monitor()
 
-    ##
-    await driver.houseKeeping.selectDAC()
-    await driver.houseKeeping.writeADCDACBytes([0xAB,0xCD])
     await Timer(50, units="us")
-    assert((await driver.houseKeeping.getADCBytesCount())==0)
 
+    ## Read first ADC
+    await read_adc_channels(driver,adc = 1, firstChannel = 0,count = 1)
+    await read_adc_channels(driver,adc = 2, firstChannel = 0,count = 8)
+    #await read_adc_channels(driver,adc = 3, channels = 8)
 
-    slaveByte = await slave.getByte()
-    print("Slave Byte: ",slaveByte)
-    assert (slaveByte) == 0xAB
-    assert (await slave.getByte()) == 0xCD
-
-    await driver.close()
     await Timer(50, units="us")
