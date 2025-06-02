9f4125d8d880e68a9ef4f2f6733cac99ed39b636
Adam Schoenwald <adam.schoenwald@gmail.com>
2025-06-02 12:17:29 -0400

    add hk eqns

--- sw/scripts/hk_loop.py
+++ sw/scripts/hk_loop.py
@@ -13,7 +13,7 @@ import asyncio
 import drivers.boards
 
 import binascii
-
+import drivers.compair.housekeeping_equations as hk_eqns
 async def callHK():
     """
     Calls housekeeping from TI ADC128S102 ADC. Loops over each of the 8 input channels.
@@ -25,7 +25,7 @@ async def callHK():
     Option for writing as LSB or MSB
     """
     ## Open UART Driver for CMOD
-    driver = drivers.boards.getCMODUartDriver("COM15")
+    driver = drivers.boards.getCMODUartDriver("COM10")
     
     await driver.open() 
     
@@ -36,16 +36,20 @@ async def callHK():
     ## Loop over ADC Settings
     for adc_num in [1,2,3]:
         for chan in range(0,8):
-            await driver.houseKeeping.selectADC(0)
-            await driver.houseKeeping.writeADCDACBytes(bytes([chan<<3,0])) # Roll chan left 3 bits per ADC128S102 datasheet
-            await driver.houseKeeping.selectADC(adc_num)
-            adcBytesCount = await driver.houseKeeping.getADCBytesCount()
-            adcBytes = await driver.houseKeeping.readADCBytes(adcBytesCount) 
-            print(f"ADC, {adc_num}, CH, {chan}, Dec, {int.from_bytes(adcBytes)}")
-            
-            #sleep between read/writes (seconds)
-            task = asyncio.create_task(asyncio.sleep(1)) 
-            await task
+            for retry in range(2):
+                print(f"try {retry}")
+                await driver.houseKeeping.selectADC(0)
+                await driver.houseKeeping.writeADCDACBytes(bytes([chan<<3,0])) # Roll chan left 3 bits per ADC128S102 datasheet
+                await driver.houseKeeping.selectADC(adc_num)
+                adcBytesCount = await driver.houseKeeping.getADCBytesCount()
+                adcBytes = await driver.houseKeeping.readADCBytes(adcBytesCount) 
+                (units, shortname) = hk_eqns.get_info(adc_num-1,chan)
+                value = hk_eqns.convert(adc_num-1,chan,int.from_bytes(adcBytes))
+                print(f"ADC, {adc_num}, CH, {chan}, Dec, {int.from_bytes(adcBytes)}, Name, {shortname}, Value, {value}, Units, {units}")
+                
+                #sleep between read/writes (seconds)
+                task = asyncio.create_task(asyncio.sleep(1)) 
+                await task
     
     await driver.close()
 
